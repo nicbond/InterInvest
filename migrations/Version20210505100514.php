796ac47db281e4a5c288ebc97c7cc5d856b6e331
nicbond <nicolasmartins78650@free.fr>
2021-05-05 12:10:38 +0200

    Add Historical Entity

---
+++ migrations/Version20210505100514.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210505100514 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE historical_company (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, siren_number VARCHAR(15) NOT NULL, city_registration VARCHAR(255) NOT NULL, date_registration DATETIME NOT NULL, share_social INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE historical_company');
+    }
+}
