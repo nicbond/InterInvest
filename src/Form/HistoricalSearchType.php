f4d5323e8508e99ce83599dc57087711f33ceaa0
nicbond <nicolasmartins78650@free.fr>
2021-05-06 14:18:48 +0200

    Add form for HistoricalSearch

---
+++ src/Form/HistoricalSearchType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\HistoricalSearch;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class HistoricalSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('created_at', DateTimeType::class, [
+                'required' => false,
+                'label' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => HistoricalSearch::class,
+            'method' => 'get',
+            'csrf_protection' => false
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
